1f9ea852ec399398133c6539f51ef0a81ceabe8c
arebuelta <42614989+arebuelta@users.noreply.github.com>
2018-09-21 00:17:01 -0700

    Update lab919

--- Labs/Lab919Collision/Lab/sketch.js
+++ Labs/Lab919Collision/Lab/sketch.js
@@ -6,26 +6,27 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20);
-  loadBalls(25);
+  loadBoids(55);
+  
 }
 function draw() {
   background(20, 20, 20);
-  yellowTriangle.run();
-  for (i = 0; i < boids.length; i++){
+  for (var i = 0; i < boids.length; i++){
   boids[i].run();
   }
+  yellowTriangle.run();
 }
-function loadBalls(numBalls){
-  for (i = 0; i < numBalls; i++){
-	  var location = createVector(random(width), random(height));
-	  var velocity = createVector(random(-3, 3), random(-3, 3));
-    var r = random(20, 30, 40);
-    var col = color(random(255), random(255), random(255));
-    boids.push(new Ball(location, velocity, r, col));
-  }
+
+function loadBoids(numBoids) {
+	for (var i = 0; i < numBoids; i++){
+		var location = createVector(random(width), random(height));
+		var velocity = createVector(random(-3, 3), random(-3, 3));
+		var col = color(100, 100, 100);
+		boids.push(new Boids(location, velocity, col));
+	}
 var location = createVector(width/2, height/2);
 var velocity = createVector(0,0);
-var r = 55;
-var col = color(255,0,0);
-yellowTriangle = new Ball(location, velocity, r, col);
+var col = color(255, 255 ,0);
+yellowTriangle = new Boids(location, velocity, col);
+console.log(boids.length);
 }
