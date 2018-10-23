910620eac120eb1787b0b78f7763d2be582cbf82
arebuelta <42614989+arebuelta@users.noreply.github.com>
2018-10-23 08:48:56 -0700

    changes

--- Labs/Lab1022/sketch.js
+++ Labs/Lab1022/sketch.js
@@ -1,14 +1,13 @@
-/* 
+/*
 ** Bubble Sort
 ** Anthony Rebuelta
 ** October 22 2018
 */
 
 // Global variables
-var words = [];
-var txt1;
-var txt2;
-var x;
+var txt1; // variable holding original set of words
+var txt2; // variable holding organized set of words
+// preload code that receives words from text file
 function preLoad(){
 	txt1 = loadStrings('words.txt');
 	txt2 = loadStrings('words.txt');
@@ -18,31 +17,36 @@ function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20);
-  preLoad();
-  x = 1;
+  preLoad(); // calls preLoad
 }
 // draw function that runs objects
 function draw(){
   fill (255, 255, 255)
   background(20, 20, 20);
   fill(255, 255, 255);
+	// writes text stating original text
   textSize(30);
   text("Original:", 25, 50);
   text(txt1, 140, 50);
+	// for loop that goes through all words
   for (var i = txt2.length-1; i >= 1; i--){
+		/* for loop that checks if each word is in alphabetical order
+		** and swaps them if they are not */
 	  for (var j = 0; j < txt2.length; j++){
 		  if (txt2[j] > txt2[j+1]){
 			  swap(txt2, j, j+1);
 		  }
 	  }
   }
+	// writes text stating text in alphabetical order
   fill(255, 255, 255);
-  text("Rearranged:", 0, 550);
+  text("Rearranged: ", 0, 550);
   text(txt2, 170, 550);
 }
+// swap function that swaps words
 function swap(txt2, a, b){
-	var old;
+	var old; // variable holding old value of txt2[a]
 	old = txt2[a];
-	txt2[a] = txt2[b]	
-	txt2[b] = old;
+	txt2[a] = txt2[b]; // makes txt2[a] equal to txt2[b]
+	txt2[b] = old; // makes txt2[b] equal to variable old, which is the old value of txt2[a]
 }
