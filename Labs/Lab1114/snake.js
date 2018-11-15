7494870058828313bae3a22ba83d460e7b0a8016
arebuelta <42614989+arebuelta@users.noreply.github.com>
2018-11-15 08:51:26 -0800

    snake

--- Labs/Lab1114/snake.js
+++ Labs/Lab1114/snake.js
@@ -2,6 +2,7 @@
 function Snake(loc, vel){
   this.loc = loc;
   this.vel = vel;
+  this.segments = [];
 
   this.run = function (){
     this.update();
@@ -11,6 +12,7 @@ function Snake(loc, vel){
     this.loc.add(this.vel);
     this.loc.x = constrain(this.loc.x, 0, width-w);
     this.loc.y = constrain(this.loc.y, 0, height-w);
+    // add segments on to snake
   }
   this.render = function(){
     fill (0, 255, 0);
